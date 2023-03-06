18b9dfb340b4422af45e0c1e6ce2478c769c2952
Viktor_Kirin <razeone1@gmail.com>
2023-03-06 23:54:05 +0100

    add react

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import'./Hello';
+import './App.css';
+import Hello from './Hello';
+
+function App() {
+  return (
+    <div style={{
+      backgroundColor: '#46b1df',
+      height: '100px',
+      fontFamily: 'sans-serif',
+      fontSize: '30px'
+      }}>
+    <Hello> </Hello>
+    
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ src/Hello.jsx
@@ -0,0 +1,28 @@
+function Hello() {
+     return (
+     <>
+          <div style={{
+          backgroundColor: '#46b1df',
+          height: '100px',
+          fontFamily: 'sans-serif',
+          fontSize: '30px',
+          boxSizing: 'border-box'
+          }}>
+          <h2 style={{
+          margin: "0px"
+          }}>Hello React</h2>
+          <button style={{
+               height: '25px',
+               width: '100px',
+               fontSize: '20px',
+               marginTop: '23px',
+               borderRadius: '5px',
+          }}>Button</button>
+          </div>
+
+     </>
+     );
+}
+    
+    export default Hello;
+    
