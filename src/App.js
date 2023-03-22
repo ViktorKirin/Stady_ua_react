dd38c23123bd72833cf2b1332345c8182cf66491
Viktor_Kirin <razeone1@gmail.com>
2023-03-22 19:28:28 +0100

    add branch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from "react";
+import LeftSide from './components/left_side/leftSide';
+import RightSide from './components/right_side/inputs/rightSide';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+     return (
+      <div className='container'>
+        <LeftSide></LeftSide>
+        <RightSide></RightSide>
+      </div>
 
-export default App;
+     )
+   }
+
+export default App
